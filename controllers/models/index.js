a366397676cd119d0199fe17d1bb841b50223b1e
dave-turnbull <dav-tu@hotmail.com>
2024-04-17 12:43:46 +0100

    added queries to articles endpoint

--- controllers/models/index.js
+++ controllers/models/index.js
@@ -1,5 +1,6 @@
 const db = require("../../db/connection.js");
-var fs = require('fs'); 
+const fs = require('fs'); 
+const format = require('pg-format');
 
 exports.retrieveEndpoints = () => {
     return fs.promises.readFile('endpoints.json', 'utf8')
@@ -13,7 +14,9 @@ const retrieveData = (sqlQuery) => {
     return db.query(sqlQuery)
     .then((result) => {
         if(result.rows.length === 0) {
-            const dataName = result.fields[0].name.match(/^[a-z]+/i)[0]
+            const dataName = result.fields
+                .find(field => field.columnID === 1).name
+                .match(/[a-z]+/ig)[0]
             return Promise.reject({status: 404, message: `${dataName} not found`})
         }
         return result
@@ -30,19 +33,40 @@ exports.retrieveArticlesById = (id) => {
     return retrieveData(sqlQuery)
 }
 
-exports.retrieveArticles = () => {
+exports.retrieveArticles = (query) => {
     //COALESCE converts the value to 0 if its null
     //AS comment_count sets the column name
     //CAST(... AS INT) converts to an interger (apparently GROUP BY returns as a VARCHAR)
     //COUNT(*) counts all the items in the group
     //GROUP BY creates groups for all comments seperated by article_id
     //SQL hurts
-    const sqlQuery = `SELECT author, title, articles.article_id, topic, created_at, votes, article_img_url, COALESCE(comments.comment_count, 0) AS comment_count FROM articles
+    let sqlQuery = format(`SELECT author, title, articles.article_id, topic, created_at, votes, article_img_url, COALESCE(comments.comment_count, 0) AS comment_count FROM articles
     LEFT JOIN (
         SELECT comments.article_id, CAST(COUNT(*) AS INT) AS comment_count
         FROM comments
         GROUP BY comments.article_id
-    ) comments ON comments.article_id = articles.article_id ORDER BY created_at DESC`
+    ) comments ON comments.article_id = articles.article_id`)
+    if (Object.keys(query).length !== 0) {
+        const approvedQueries = [
+            'article_id', 
+            'title', 
+            'topic', 
+            'author',
+            'created_at', 
+            'votes', 
+            'article_img_url'
+        ]
+        const conditionalsArray = []
+        for (key in query) {
+            if (approvedQueries.includes(key)) {
+                conditionalsArray.push(format(`%I = %L`, key, query[key]))
+            } else {
+                return Promise.reject({status: 400, message: "Invalid query"})
+            }
+        }
+        sqlQuery += ` WHERE ${conditionalsArray.join(' AND ')}`
+    }
+    sqlQuery += format(` ORDER BY created_at DESC`)
     return retrieveData(sqlQuery)
 }
 
