ee28855d7e0ad9f37f42dec25b14031ec2bec397
dave-turnbull <dav-tu@hotmail.com>
2024-05-07 09:22:51 +0100

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 const { handleApiErrors } = require('./errorHandling')
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 app.use('/api', apiRouter)
